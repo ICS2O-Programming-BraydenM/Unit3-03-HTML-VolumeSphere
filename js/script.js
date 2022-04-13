9d8ed725e1fb4559de1fc4d604a417cb6b1c8bc0
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-04-13 01:38:23 +0000

    updated all files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Brayden MacMillan All rights reserved
+// Created by: Brayden MacMillan
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calcVolume () {
+  // get user input
+  let side_a = parseFloat(document.getElementById("side-a").value)
+	let side_b = parseFloat(document.getElementById("side-b").value)
+	let height = parseFloat(document.getElementById("height").value)
+
+
+  // calculate the volume
+  let volume = (1/6) * side_a * side_b * height
+
+  // display the results
+  document.getElementById("volume").innerHTML = "The volume is " + volume.toFixed(2) + "m<sup>3</sup>"
+}
